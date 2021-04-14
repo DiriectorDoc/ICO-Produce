4b0900e40a9d8196c28924093e531da42b6759e2
Diriector_Doc <davijust213@yahoo.ca>
2021-04-14 19:19:57 -0400

    Working prototype?

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import * as Magick from "./node_modules/wasm-imagemagick/dist/magickAPI.js";
+import * as Magick from "./node_modules/wasm-imagemagick/dist/magickAPI.js"
+//import * as Magick from "https://cdn.jsdelivr.net/npm/wasm-imagemagick/dist/bundles/magickApi.js"
 
 async function command(files, command){
     return await Magick.Call(files, command.split(" "))
@@ -6,7 +7,7 @@ async function command(files, command){
 
 const /*args = {},*/
       preparedImages = [],
-      inputImages = [];
+      preparedFiles = [];
 
 /*for(const [k, v] of new URLSearchParams(location.href)){
     args[k] = v
@@ -22,34 +23,40 @@ $(function(){
             this.name = name;
         },
         imagesToICO = async function(inputFiles){
-            const cmd = "convert";
+            $(".output-images .loading").show()
+            $(".output-images img:not(.loading)").remove()
+            let cmd = "convert";
             for(let e of inputFiles){
-                cmd += e.name
+                cmd += " "+e.name
             }
-            cmd += "icon.jpg";
-            let processedFile = (await command(inputFiles, cmd))[0]
-            $(".input-images").append(`<img src="${URL.createObjectURL(processedFile.blob)}">`)
-            console.log("created image " + processedFile.name)
+            cmd += " icon.ico";
+            let processedFile = (await command(inputFiles, cmd))[0];
+            $(".output-images").append(`<img src="${URL.createObjectURL(processedFile.blob)}" />`)
+            
+            $(".output-images .loading").hide()
         };
     $("input").change(async function(){
-        $(".loading").show()
-        preparedImages.length = inputImages.length = 0;
+        $(".input-images .loading").show()
+        $(".input-images img:not(.loading)").remove()
+        preparedImages.length = 0;
         for(let e of this.files){
             let name = e.name.replaceAll(" ", "_");
             await new Promise((resolve, reject) => {
                 let img = new Image();
                 img.onload = async () => {
+                    let buffer;
+                    await e.arrayBuffer().then(arrayBuffer => {
+                        buffer = arrayBuffer
+                    });
                     if(img.width != img.height || img.width > 256){
-                        let buffer,
-                            max = Math.max(img.width, img.height),
+                        let max = Math.max(img.width, img.height),
                             aspect = max >= 256 ? 256 : max;
+                        e = (await command([new U8File(buffer, name)], `convert -background none -gravity center ${name} -resize ${aspect}x${aspect} -extent ${aspect}x${aspect} ${name}.png`))[0].blob;
                         await e.arrayBuffer().then(arrayBuffer => {
                             buffer = arrayBuffer
                         });
-                        e = (await command([new U8File(buffer, name)], `convert -background none -gravity center ${name} -resize ${aspect}x${aspect} -extent ${aspect}x${aspect} ${name}.png`))[0];
-                        img.src = URL.createObjectURL(e.blob)
                     }
-                    inputImages.push(img)
+                    preparedFiles.push(new U8File(buffer, name))
                     resolve()
                 };
                 img.onerror = reject;
@@ -58,9 +65,13 @@ $(function(){
             preparedImages.push(e)
         }
         for(let e of preparedImages){
-            $(".input-images").append(`<img src="${URL.createObjectURL(e.blob ?? e)}" />`)
+            $(".input-images").append(`<img src="${URL.createObjectURL(e)}" />`)
         }
-        $(".loading").hide()
-        //imagesToICO(inputFiles)
+        $(".input-images .loading").hide()
+        
+        $(".convert.button").removeClass("gray")
+        $("button").click(function(){
+            imagesToICO(preparedFiles)
+        })
     })
 })
